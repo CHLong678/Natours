f731d478abf9c908e9944e259988a3315aea2d64
CHLong2503 <caolong2503@gmail.com>
2024-02-16 20:22:04 +0700

    Init

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/v1/tours', );
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Starting server on port ${port}`);
+});
