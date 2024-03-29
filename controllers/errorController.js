738b99b649eb018635d1bcc39da7c374242907ea
CHLong2503 <caolong2503@gmail.com>
2024-03-29 14:16:08 +0700

    Update handle error db

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,66 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.keyValue.name;
+  // console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data -> ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    // console.error('ERROR:', err);
+
+    // Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err, name: err.name };
+    // console.log(error);
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000 && error.keyPattern && error.keyPattern.name === 1)
+      error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
+  }
 };
--- utils/appError.js
+++ utils/appError.js
@@ -1,10 +1,11 @@
 class AppError extends Error {
-  constructor(message, statusCode) {
+  constructor(message, statusCode, isPublic = true) {
     super(message);
 
     this.statusCode = statusCode;
     this.status = `${this.statusCode}`.startsWith('4') ? 'fail' : 'error';
-    this.isOprational = true;
+    this.isOperational = true;
+    this.isPublic = isPublic;
 
     Error.captureStackTrace(this, this.constructor);
   }
